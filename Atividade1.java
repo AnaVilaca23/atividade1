9a7d34b62e59a481bcffeda22ab854636fc40dc4
AnaVilaca23 <72093795+AnaVilaca23@users.noreply.github.com>
2020-09-29 18:34:49 -0300

    Add files via upload

---
+++ Atividade1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade1;
+
+/**
+ *
+ * @author Ana Vilaca
+ */
+public class Atividade1 {
+
+    //lista 1- atividade 1
+    public static void main(String[] args) {
+        System.out.println("hi lorena");
+    }
+
+}
